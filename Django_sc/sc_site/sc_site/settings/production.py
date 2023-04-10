57cdf55e952396ca54881548705f2d5615255c39
toya-shiinoki <toya.go.go.go@gmail.com>
2023-04-10 04:29:11 +0000

    pro追記

--- Django_sc/sc_site/sc_site/settings/production.py
+++ Django_sc/sc_site/sc_site/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'django-insecure-*6^b_9ztmlefnjk8_2+e+0@jfw0z7p%xeu#nkjj33hob2km#!c
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Django',
+        'NAME': 'Django_pro',
         'USER': 'Django_pro_server',
         'PASSWORD': 'hK8X7C_(',
         'HOST': '192.168.101.137',
